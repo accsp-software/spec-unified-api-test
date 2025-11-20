abe717170aa6a95a58bfd0676b52f006219183a1
Ed Martino <47990520+marty2626@users.noreply.github.com>
2025-11-20 17:49:17 -0500

    Update static file serving path in main.js

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,7 @@
 var express = require("express")
 
 var app = express();
-var docs_handler = express.static(__dirname + '/docs/');
+var docs_handler = express.static(__dirname + '/');
 app.use(docs_handler);
 //  start the server
 app.listen(8002);
